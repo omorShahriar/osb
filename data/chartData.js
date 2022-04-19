22fe752dbab257b4d8faf6f62a014c2bafbcfc47
omor shahriar <omor.shahriar2000@gmail.com>
2022-04-19 15:12:54 +0600

    graph color changed

--- data/chartData.js
+++ data/chartData.js
@@ -3,7 +3,7 @@ export const incomeData = {
   datasets: [
     {
       label: "net income ($Millions)",
-      backgroundColor: "#00945F",
+      backgroundColor: "#2563eb",
       borderColor: "rgb(255, 255, 255)",
       data: [1920000, 1737000, 3759000, 1889000, 3000000],
     },
@@ -14,7 +14,7 @@ export const bookValueData = {
   datasets: [
     {
       label: "Book Value per Share",
-      backgroundColor: "#00945F",
+      backgroundColor: "#2563eb",
       borderColor: "rgb(255, 255, 255)",
       data: [34.07, 34.1, 39.82, 42.56, 43.88],
     },
@@ -25,7 +25,7 @@ export const totalAssetsData = {
   datasets: [
     {
       label: "Total Assets ($Millions)",
-      backgroundColor: "#00945F",
+      backgroundColor: "#2563eb",
       borderColor: "rgb(255, 255, 255)",
       data: [355028000, 349811000, 399521000, 494536000, 571142000],
     },
@@ -36,7 +36,7 @@ export const totalLoansData = {
   datasets: [
     {
       label: "Total Loans ($Millions)",
-      backgroundColor: "#00945F",
+      backgroundColor: "#2563eb",
       borderColor: "rgb(255, 255, 255)",
       data: [169235000, 224240000, 253165000, 330034000, 303736000],
     },
@@ -47,7 +47,7 @@ export const totalDepositsData = {
   datasets: [
     {
       label: "Total Deposits ($Millions)",
-      backgroundColor: "#00945F",
+      backgroundColor: "#2563eb",
       borderColor: "rgb(255, 255, 255)",
       data: [324096000, 318497000, 361523000, 444702063, 0],
     },
